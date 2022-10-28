54faa29c9d79f81ddd81555e14198bd558b2e635
LuisBM18 <016100201a@uandina.edu.pe>
2022-10-28 17:57:58 -0500

    OK

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'almacen1'`, () => {
+  it(`should have as title 'BELU'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('almacen1');
+    expect(app.title).toEqual('BELU');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('almacen1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('BELU app is running!');
   });
 });
